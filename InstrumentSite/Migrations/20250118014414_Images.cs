ddfc92a238c1ab75e1ea16a499abe21f322f99d4
ZeroVik <alexkacov1912@gmail.com>
2025-01-18 04:16:57 +0200

    Files added

---
+++ InstrumentSite/Migrations/20250118014414_Images.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace InstrumentSite.Migrations
+{
+    /// <inheritdoc />
+    public partial class Images : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "CreatedAt", "PasswordHash", "UpdatedAt" },
+                values: new object[] { new DateTime(2025, 1, 18, 1, 44, 14, 649, DateTimeKind.Utc).AddTicks(4357), "$2a$11$jHyTjutoGesr7EEJzFIs3OgWrDn4CfG9eW7i.5A9AspcEy.WCOny.", new DateTime(2025, 1, 18, 1, 44, 14, 649, DateTimeKind.Utc).AddTicks(4362) });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "CreatedAt", "PasswordHash", "UpdatedAt" },
+                values: new object[] { new DateTime(2025, 1, 17, 22, 48, 30, 914, DateTimeKind.Utc).AddTicks(1634), "$2a$11$HAPz25iFVlyFZ1WGOSYfYOb.LgyYme06vl0argz1Iarolrmo74IfC", new DateTime(2025, 1, 17, 22, 48, 30, 914, DateTimeKind.Utc).AddTicks(1639) });
+        }
+    }
+}
---
+++ InstrumentSite/Utilities/SaveImageUtil.cs
@@ -0,0 +1,27 @@
+﻿namespace InstrumentSite.Utilities
+{
+    public class SaveImageUtil
+    {
+        public static async Task<string> SaveImageFileAsync(IFormFile imageFile)
+        {
+            var uploadsDirectory = Path.Combine("uploads", "products");
+            if (!Directory.Exists(uploadsDirectory))
+            {
+                Directory.CreateDirectory(uploadsDirectory);
+            }
+
+            // Generate a unique file name
+            var fileName = $"{Guid.NewGuid()}{Path.GetExtension(imageFile.FileName)}";
+            var filePath = Path.Combine(uploadsDirectory, fileName);
+
+            using (var stream = new FileStream(filePath, FileMode.Create))
+            {
+                await imageFile.CopyToAsync(stream);
+            }
+
+            // Return relative path
+            return Path.Combine("uploads/products", fileName).Replace("\\", "/");
+        }
+
+    }
+}
